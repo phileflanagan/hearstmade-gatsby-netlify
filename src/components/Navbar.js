7b57f4d3235aa5fef0edb283978bece4b9f528dd
Phil Flanagan <phileflanagan@gmail.com>
2020-01-10 12:57:34 -0500

    Fix case study and mobile nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,105 +6,102 @@ import logo from '../brand/logos/logo.svg';
 import './Navbar.sass';
 
 const Navbar = class extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            active: false,
-            navBarActiveClass: '',
-            visible: true,
-            prevScrollPos: 99999
-        };
-    }
-    componentDidMount() {
-        window.addEventListener('scroll', this.handleScroll);
-    }
+	constructor(props) {
+		super(props);
+		this.state = {
+			active: false,
+			navBarActiveClass: '',
+			visible: true,
+			prevScrollPos: 99999
+		};
+	}
+	componentDidMount() {
+		window.addEventListener('scroll', this.handleScroll);
+	}
 
-    // Remove the event listener when the component is unmount.
-    componentWillUnmount() {
-        window.removeEventListener('scroll', this.handleScroll);
-    }
+	// Remove the event listener when the component is unmount.
+	componentWillUnmount() {
+		window.removeEventListener('scroll', this.handleScroll);
+	}
 
-    toggleHamburger = () => {
-        // toggle the active boolean in the state
-        this.setState(
-            {
-                active: !this.state.active
-            },
-            // after state has been updated,
-            () => {
-                // set the class in state for the navbar accordingly
-                this.state.active
-                    ? this.setState({
-                          navBarActiveClass: 'is-active'
-                      })
-                    : this.setState({
-                          navBarActiveClass: ''
-                      });
-            }
-        );
-    };
+	toggleHamburger = () => {
+		// toggle the active boolean in the state
+		this.setState(
+			{
+				active: !this.state.active
+			},
+			// after state has been updated,
+			() => {
+				// set the class in state for the navbar accordingly
+				this.state.active
+					? this.setState({
+							navBarActiveClass: 'is-active'
+						})
+					: this.setState({
+							navBarActiveClass: ''
+						});
+			}
+		);
+	};
 
-    handleScroll = () => {
-        const { prevScrollPos } = this.state;
+	handleScroll = () => {
+		const { prevScrollPos } = this.state;
+		const currentScrollPos = window.pageYOffset;
+		if (currentScrollPos < 450) {
+			this.setState({
+				prevScrollPos: currentScrollPos,
+				visible: true
+			});
+		} else if (prevScrollPos < currentScrollPos) {
+			this.setState({
+				prevScrollPos: currentScrollPos,
+				visible: false
+			});
+		} else {
+			this.setState({
+				prevScrollPos: currentScrollPos,
+				visible: true
+			});
+		}
+	};
 
-        const currentScrollPos = window.pageYOffset;
-        const visible = prevScrollPos > currentScrollPos;
-
-        this.setState({
-            prevScrollPos: currentScrollPos,
-            visible
-        });
-    };
-
-    render() {
-        return (
-            <nav
-                className={`navbar ${
-                    !this.state.visible ? 'navbar__hidden' : ''
-                }`}
-                role="navigation"
-                aria-label="main-navigation"
-            >
-                <div className="navbar-brand">
-                    <Link to="/" className="navbar-item centerY" title="Logo">
-                        <img
-                            src={logo}
-                            alt="Hearstmade"
-                            style={{ margin: 0 }}
-                        />
-                    </Link>
-                    {/* Hamburger menu */}
-                    <div
-                        className={`burger ${this.state.navBarActiveClass}`}
-                        onClick={() => this.toggleHamburger()}
-                    >
-                        <span />
-                        <span />
-                        <span />
-                    </div>
-                </div>
-                <div
-                    id="navMenu"
-                    className={`menu ${this.state.navBarActiveClass}`}
-                >
-                    <div className="oa centerXY flex-column fill">
-                        <Link className="navbar-item" to="/">
-                            Home
-                        </Link>
-                        <Link className="navbar-item" to="/about">
-                            About
-                        </Link>
-                        <Link className="navbar-item" to="/casestudy">
-                            Case Studies
-                        </Link>
-                        <Link className="navbar-item" to="/contact">
-                            Contact
-                        </Link>
-                    </div>
-                </div>
-            </nav>
-        );
-    }
+	render() {
+		return (
+			<nav
+				className={`navbar ${!this.state.visible ? 'navbar__hidden' : ''}`}
+				role="navigation"
+				aria-label="main-navigation"
+			>
+				<div className="navbar-brand">
+					<Link to="/" className="navbar-item centerY" title="Logo">
+						<img src={logo} alt="Hearstmade" style={{ margin: 0 }} />
+					</Link>
+					{/* Hamburger menu */}
+					<div className={`burger ${this.state.navBarActiveClass}`} onClick={() => this.toggleHamburger()}>
+						<span />
+						<span />
+						<span />
+					</div>
+				</div>
+				<div id="navMenu" className={`menu ${this.state.navBarActiveClass}`}>
+					<div className="oa centerXY flex-column fill">
+						<Link className="navbar-item" to="/">
+							Home
+						</Link>
+						<Link className="navbar-item" to="/about">
+							About
+						</Link>
+						<Link className="navbar-item" to="/casestudy">
+							Case Studies
+						</Link>
+						<Link className="navbar-item" to="/contact">
+							Contact
+						</Link>
+					</div>
+				</div>
+			</nav>
+		);
+	}
 };
 
 export default Navbar;
