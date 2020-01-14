fffd29c091bdadcf1c47e92fbde349e8a760e64a
unknown <phileflanagan@gmail.com>
2020-01-14 00:11:02 -0500

    Add service chart

--- src/components/ServiceChart.js
+++ src/components/ServiceChart.js
@@ -7,11 +7,79 @@ const ServiceChart = () => {
     return (
         <section className="light vertPadding">
             <div className="container service-chart">
-                <p className="chart-header">
+                <h1 className="chart-header">
                     A full service offering that
                     <br />
                     delivers what clients want
-                </p>
+                </h1>
+                <section class="box">
+                    <div className="subbox">
+                        <h4 className="m0">Strategy</h4>
+                        <ul>
+                            <li>
+                                Creative, Audience Engagement, Distribution,
+                                Revenue Generation
+                            </li>
+                        </ul>
+                    </div>
+                </section>
+                <section className="box boxlist">
+                    <div className="subbox">
+                        <h4 className="m0">Content Creation</h4>
+                        <ul>
+                            <li>Video</li>
+                            <li>Social</li>
+                            <li>Magazine</li>
+                            <li>Digital Display</li>
+                        </ul>
+                    </div>
+                    <div className="subbox">
+                        <h4 className="m0">Production</h4>
+                        <ul>
+                            <li>Studio Space</li>
+                            <li>Post Production</li>
+                            <li>Facilities</li>
+                            <li>Talent Bookers</li>
+                        </ul>
+                    </div>
+                    <div className="subbox">
+                        <h4 className="m0">Digital Marketing</h4>
+                        <ul>
+                            <li>Site Design / UX</li>
+                            <li>Performance Media</li>
+                            <li>SEO</li>
+                            <li>Technology / Dev</li>
+                            <li>Digital Display</li>
+                        </ul>
+                    </div>
+                    <div className="subbox">
+                        <h4 className="m0">Distribution</h4>
+                        <ul>
+                            <li>Circulation</li>
+                            <li>Fulfillment</li>
+                            <li>Subscription Management</li>
+                            <li>Loyalty</li>
+                        </ul>
+                    </div>
+                    <div className="subbox">
+                        <h4 className="m0">Monetization</h4>
+                        <ul>
+                            <li>Advertising Sales</li>
+                            <li>Programmatic Optimization</li>
+                            <li>Subscription Strategy</li>
+                        </ul>
+                    </div>
+                </section>
+                <section class="box">
+                    <div className="subbox">
+                        <h4 className="m0">
+                            Proprietary Tools &amp; Technology
+                        </h4>
+                        <ul>
+                            <li>Data Studio, MediaOS, Pubworx, CDS</li>
+                        </ul>
+                    </div>
+                </section>
             </div>
         </section>
     );
