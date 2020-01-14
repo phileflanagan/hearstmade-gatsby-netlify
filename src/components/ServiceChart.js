f4ce2f279d5826a0325c68c84b286b3371162008
unknown <phileflanagan@gmail.com>
2020-01-13 22:47:10 -0500

    Set up boilerplate for services

---
+++ src/components/ServiceChart.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'gatsby';
+
+import './ServiceChart.sass';
+
+const ServiceChart = () => {
+    return (
+        <section className="light vertPadding">
+            <div className="container service-chart">
+                <p className="chart-header">
+                    A full service offering that
+                    <br />
+                    delivers what clients want
+                </p>
+            </div>
+        </section>
+    );
+};
+
+export default ServiceChart;
--- src/templates/services-page.js
+++ src/templates/services-page.js
@@ -5,6 +5,7 @@ import Helmet from 'react-helmet';
 
 import Stats from '../components/Stats';
 import Contacts from '../components/Contacts';
+import ServiceChart from '../components/ServiceChart';
 
 export const ServicesPageTemplate = () => <div />;
 
@@ -19,28 +20,7 @@ const ServicesPage = () => {
                     content={`See the services HearstMade can provide`}
                 />
             </Helmet>
-            <section className="light vertPadding">
-                <div className="container">
-                    <h1>Services</h1>
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                        Vestibulum commodo maximus maximus. Donec tincidunt est
-                        libero, et imperdiet erat egestas a. Vivamus malesuada
-                        sapien et consequat sodales. Suspendisse dapibus tellus
-                        a ligula accumsan, tristique pharetra metus facilisis.
-                        Vestibulum ante ipsum primis in faucibus orci luctus et
-                        ultrices posuere cubilia Curae; Etiam id venenatis eros.
-                        Sed elit dui, mattis porttitor tristique id, varius ut
-                        risus. Proin vitae sollicitudin odio, vel volutpat
-                        lacus. Cras consequat quam eget mauris mattis, sit amet
-                        scelerisque orci finibus. Sed ullamcorper mattis eros
-                        eget volutpat. Interdum et malesuada fames ac ante ipsum
-                        primis in faucibus. Quisque id massa lacus. In hac
-                        habitasse platea dictumst. Etiam hendrerit semper nulla
-                        vitae rutrum.
-                    </p>
-                </div>
-            </section>
+            <ServiceChart />
             <Stats />
             <Contacts />
         </Layout>
